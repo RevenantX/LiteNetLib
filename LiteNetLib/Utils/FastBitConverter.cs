b447d83ba03f212ff45f32f60062c9547771e5cb
RevenantX <doomrevx@gmail.com>
2016-03-17 14:18:02 +0200

    fix indent.

--- LiteNetLib/Utils/FastBitConverter.cs
+++ LiteNetLib/Utils/FastBitConverter.cs
@@ -27,14 +27,14 @@ namespace LiteNetLib.Utils
         public static void WriteLittleEndian(byte[] buffer, int offset, ulong data)
         {
 #if BIGENDIAN
-        buffer[offset + 7] = (byte)(data);
-        buffer[offset + 6] = (byte)(data >> 8);
-        buffer[offset + 5] = (byte)(data >> 16);
-        buffer[offset + 4] = (byte)(data >> 24);
-        buffer[offset + 3] = (byte)(data >> 32);
-        buffer[offset + 2] = (byte)(data >> 40);
-        buffer[offset + 1] = (byte)(data >> 48);
-        buffer[offset    ] = (byte)(data >> 56);
+            buffer[offset + 7] = (byte)(data);
+            buffer[offset + 6] = (byte)(data >> 8);
+            buffer[offset + 5] = (byte)(data >> 16);
+            buffer[offset + 4] = (byte)(data >> 24);
+            buffer[offset + 3] = (byte)(data >> 32);
+            buffer[offset + 2] = (byte)(data >> 40);
+            buffer[offset + 1] = (byte)(data >> 48);
+            buffer[offset    ] = (byte)(data >> 56);
 #else
             buffer[offset] = (byte)(data);
             buffer[offset + 1] = (byte)(data >> 8);
@@ -50,10 +50,10 @@ namespace LiteNetLib.Utils
         public static void WriteLittleEndian(byte[] buffer, int offset, int data)
         {
 #if BIGENDIAN
-        buffer[offset + 3] = (byte)(data);
-        buffer[offset + 2] = (byte)(data >> 8);
-        buffer[offset + 1] = (byte)(data >> 16);
-        buffer[offset    ] = (byte)(data >> 24);
+            buffer[offset + 3] = (byte)(data);
+            buffer[offset + 2] = (byte)(data >> 8);
+            buffer[offset + 1] = (byte)(data >> 16);
+            buffer[offset    ] = (byte)(data >> 24);
 #else
             buffer[offset] = (byte)(data);
             buffer[offset + 1] = (byte)(data >> 8);
@@ -65,8 +65,8 @@ namespace LiteNetLib.Utils
         public static void WriteLittleEndian(byte[] buffer, int offset, short data)
         {
 #if BIGENDIAN
-        buffer[offset + 1] = (byte)(data);
-        buffer[offset    ] = (byte)(data >> 8);
+            buffer[offset + 1] = (byte)(data);
+            buffer[offset    ] = (byte)(data >> 8);
 #else
             buffer[offset] = (byte)(data);
             buffer[offset + 1] = (byte)(data >> 8);
