cfd21e0af22f8d18840171db30e1456d9edfc3fd
Yevhenii Vitiuk <kaladrius.trip@gmail.com>
2017-03-21 13:16:23 +0200

    Refactoring connection test

--- LiteNetLibUnitTests/ConnectionTest.cs
+++ LiteNetLibUnitTests/ConnectionTest.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using LiteNetLib;
 using NUnit.Framework;
 
@@ -12,82 +8,101 @@ namespace LiteNetLibUnitTests
     [TestFixture, SingleThreaded]
     public class LiteNetLibTest
     {
-        [Test]
-        public void ConnectionByIpV4()
-        {
-            var serverListener = new EventBasedNetListener();
-            var server = new NetManager(serverListener, "IpV4Test");
+        private EventBasedNetListener _serverListener = new EventBasedNetListener();
+        private EventBasedNetListener _clientListener1 = new EventBasedNetListener();
+        private EventBasedNetListener _clientListener2 = new EventBasedNetListener();
 
-            if (!server.Start(9050))
+        private NetManager _server;
+        private NetManager _client1;
+        private NetManager _client2;
+
+        private const string ServerName = "test_server";
+
+        [SetUp]
+        public void Init()
+        {
+            _server = new NetManager(_serverListener, ServerName);
+            _client1 = new NetManager(_clientListener1, ServerName);
+            _client2 = new NetManager(_clientListener2, ServerName);
+
+            if (!_server.Start(9050))
             {
                 Assert.Fail("Server start failed");
             }
-
-            var clientListener = new EventBasedNetListener();
-            var client = new NetManager(clientListener, "IpV4Test")
-            {
-                SimulationMaxLatency = 1500,
-                MergeEnabled = true
-            };
             
-            if (!client.Start())
+            if (!_client1.Start())
             {
                 Assert.Fail("Client1 start failed");
             }
 
-            bool clietnConnected = false;
-            serverListener.PeerConnectedEvent += peer =>
+            if (!_client2.Start())
+            {
+                Assert.Fail("Client2 start failed");
+            }
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _client1.Stop();
+            _client2.Stop();
+            _server.Stop();
+        }
+
+        [Test]
+        public void ConnectionByIpV4()
+        {
+            bool connected = false;
+            EventBasedNetListener.OnPeerConnected action = peer =>
             {
                 //TODO: Identify user
-                clietnConnected = true;
+                connected = true;
             };
+           
+            _serverListener.PeerConnectedEvent += action;
 
-            client.Connect("127.0.0.1", 9050);
+            _client1.Connect("127.0.0.1", 9050);
 
-            while (!clietnConnected)
+            while (!connected)
             {
                 Thread.Sleep(15);
-                server.PollEvents();
+                _server.PollEvents();
             }
+            
+            foreach (var netPeer in _client1.GetPeers())
+            {
+                _client1.DisconnectPeer(netPeer);
+            }
+            
+            _clientListener1.PeerConnectedEvent -= action;
         }
 
         [Test]
         public void ConnectionByIpV6()
         {
-            var serverListener = new EventBasedNetListener();
-            var server = new NetManager(serverListener, "IpV6");
-
-            if (!server.Start(9051))
-            {
-                Assert.Fail("Server start failed");
-            }
-
-            var clientListener = new EventBasedNetListener();
-            var client = new NetManager(clientListener, "IpV6")
-            {
-                SimulationMaxLatency = 1500,
-                MergeEnabled = true
-            };
-
-            if (!client.Start())
-            {
-                Assert.Fail("Client1 start failed");
-            }
-
-            bool clietnConnected = false;
-            serverListener.PeerConnectedEvent += peer =>
+            bool connected = false;
+            EventBasedNetListener.OnPeerConnected action = peer =>
             {
                 //TODO: Identify user
-                clietnConnected = true;
+                connected = true;
             };
 
-            client.Connect("::1", 9051);
+            _serverListener.PeerConnectedEvent += action;
 
-            while (!clietnConnected)
+            _client1.Connect("::1", 9050);
+
+            while (!connected)
             {
                 Thread.Sleep(15);
-                server.PollEvents();
+                _server.PollEvents();
             }
+
+            foreach (var netPeer in _client1.GetPeers())
+            {
+                _client1.DisconnectPeer(netPeer);
+            }
+
+            _clientListener1.PeerConnectedEvent -= action;
         }
     }
 }
