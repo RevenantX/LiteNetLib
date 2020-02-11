f99fd298b721b286d5cab2bb36bd2746785f0c09
André B. Amundsen <andre.amundsen@soprasteria.com>
2020-02-11 15:36:08 +0100

    TMP

---
+++ LiteNetLib.Tests/EncryptionLayerTest.cs
@@ -0,0 +1,100 @@
+﻿using LiteNetLib.Layers;
+using NUnit.Framework;
+using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace LiteNetLib.Tests
+{
+    [TestFixture]
+    public class EncryptionLayerTest
+    {
+        private Random _random;
+        private Stopwatch _stopwatch;
+
+        [SetUp]
+        public void Setup()
+        {
+            _random = new Random();
+            _stopwatch = new Stopwatch();
+        }
+
+        [Test]
+        public void Aes_Layer_encrypt_decrypt()
+        {
+            var keyGen = RandomNumberGenerator.Create();
+            byte[] key = new byte[AesEncryptLayer.KeySizeInBytes];
+            keyGen.GetBytes(key);
+            const string testData = "This text is long enough to need multiple blocks to encrypt";
+
+            var outboudLayer = new AesEncryptLayer(key);
+            byte[] outboundPlaintext = Encoding.ASCII.GetBytes(testData);
+            byte[] cipherText = outboundPlaintext;
+            int lengthOfPacket = outboundPlaintext.Length;
+            int start = 0;
+            int length = outboundPlaintext.Length;
+            outboudLayer.ProcessOutBoundPacket(ref cipherText, ref start, ref length);
+
+            //Console.WriteLine(BitConverter.ToString(cipherText, 0, length).Replace("-", ""));
+            Assert.That(length, Is.InRange(lengthOfPacket + AesEncryptLayer.BlockSizeInBytes, lengthOfPacket + outboudLayer.ExtraPacketSizeForLayer));
+
+            var inboundLayer = new AesEncryptLayer(key);
+            byte[] inboundPlaintext = cipherText;
+            inboundLayer.ProcessInboundPacket(ref inboundPlaintext, ref length);
+            Assert.That(inboundPlaintext, Is.EquivalentTo(Encoding.ASCII.GetBytes(testData)));
+        }
+
+        [Test]
+        [Explicit]
+        public void SpeedOfXorLayer()
+        {
+            var keyGen = RandomNumberGenerator.Create();
+            byte[] key = new byte[1500]; //As long as packet size since XOR alone is very unsafe
+            keyGen.GetBytes(key);
+            PacketLayerBase layer = new XorEncryptLayer(key);
+            byte[][] sampleData = GenerateData(10000, 1500);
+            Encrypt(sampleData, layer);
+            //Check console output for ticks spent to compare with AES
+        }
+
+        [Test]
+        [Explicit]
+        public void SpeedOfAesLayer()
+        {
+            var keyGen = RandomNumberGenerator.Create();
+            byte[] key = new byte[AesEncryptLayer.KeySizeInBytes];
+            keyGen.GetBytes(key);
+            byte[] iv = new byte[AesEncryptLayer.BlockSizeInBytes];
+            keyGen.GetBytes(iv);
+            PacketLayerBase layer = new AesEncryptLayer(key);
+            byte[][] sampleData = GenerateData(10000, 1500 - layer.ExtraPacketSizeForLayer);
+            Encrypt(sampleData, layer);
+            //Check output for ticks spent to compare with XOR
+        }
+
+        private byte[][] GenerateData(int numSamples, int sizePerSample)
+        {
+            byte[][] sampleData = new byte[numSamples][];
+            for (int i = 0; i < sampleData.Length; i++)
+            {
+                sampleData[i] = new byte[sizePerSample];
+                _random.NextBytes(sampleData[i]);
+            }
+            return sampleData;
+        }
+
+        private void Encrypt(byte[][] sampleData, PacketLayerBase layer)
+        {
+            _stopwatch.Start();
+            for (int i = 0; i <= sampleData.Length; i++)
+            {
+                int length = sampleData[i].Length;
+                int start = 0;
+                layer.ProcessOutBoundPacket(ref sampleData[i], ref start, ref length);
+            }
+            _stopwatch.Stop();
+            Console.WriteLine("Decrypt with " + layer.GetType().Name + " took " + _stopwatch.ElapsedTicks + " ticks");
+        }
+    }
+}
---
+++ LiteNetLib/Layers/AesEncryptLayer.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace LiteNetLib.Layers
+{
+    /// <summary>
+    /// Uses AES encryption in CBC mode. Make sure you handle your key properly.
+    /// GCHandle.Alloc(key, GCHandleType.Pinned) to avoid your key being moved around different memory segments.
+    /// ZeroMemory(gch.AddrOfPinnedObject(), key.Length); to erase it when you are done.
+    /// About 4 times slower than XOR, but this is secure, XOR is not. 
+    /// Why encrypt: http://ithare.com/udp-for-games-security-encryption-and-ddos-protection/
+    /// </summary>
+    public class AesEncryptLayer : PacketLayerBase
+    {
+        public const int KeySize = 256;
+        public const int BlockSize = 128;
+        public const int KeySizeInBytes = KeySize / 8;
+        public const int BlockSizeInBytes = BlockSize / 8;
+
+        private readonly AesCryptoServiceProvider _aes;
+        private ICryptoTransform _encryptor;
+        private byte[] cipherBuffer = new byte[1500]; //Max possible UDP packet size
+        private ICryptoTransform _decryptor;
+
+        /// <summary>
+        /// Should be safe against eavesdropping, but is vulnerable to tampering
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="initializationVector"></param>
+        public AesEncryptLayer(byte[] key) : base(BlockSizeInBytes * 2)
+        {
+            if (key.Length != KeySizeInBytes) throw new NotSupportedException("EncryptLayer only supports keysize " + KeySize);
+
+            //Switch this with AesGCM for better performance, requires .NET Core 3.0 or Standard 2.1
+            _aes = new AesCryptoServiceProvider
+            {
+                KeySize = KeySize,
+                BlockSize = BlockSize,
+                Key = key,
+
+                Mode = CipherMode.CBC,
+                Padding = PaddingMode.PKCS7
+            };
+            _encryptor = _aes.CreateEncryptor();
+            _decryptor = _aes.CreateDecryptor();
+        }
+
+        public override void ProcessInboundPacket(ref byte[] data, ref int length)
+        {
+            //Can't copy directly to _aes.IV. It won't work for some reason.
+            byte[] iv = new byte[_aes.IV.Length];
+            Buffer.BlockCopy(data, 0, iv, 0, iv.Length);
+            _aes.IV = iv;
+            _decryptor = _aes.CreateDecryptor();
+
+            int currentRead = iv.Length;
+            int currentWrite = 0;
+            while (length - currentRead > _decryptor.InputBlockSize)
+            {
+                int decryptedCount = _decryptor.TransformBlock(data, currentRead, _decryptor.InputBlockSize, cipherBuffer, currentWrite);
+                currentWrite += decryptedCount;
+                currentRead += decryptedCount;
+            }
+
+            byte[] lastblock = _decryptor.TransformFinalBlock(data, currentRead, length - currentRead);
+            Buffer.BlockCopy(lastblock, 0, cipherBuffer, currentWrite, lastblock.Length);
+
+            data = cipherBuffer;
+            length = currentWrite + lastblock.Length;
+        }
+
+        public override void ProcessOutBoundPacket(ref byte[] data, ref int offset, ref int length)
+        {
+            //Some Unity platforms may need these (and will be slower + generate garbage)
+            if (!_encryptor.CanReuseTransform)
+            {
+                _aes.GenerateIV();
+                _encryptor = _aes.CreateEncryptor();
+            }
+
+            //Copy IV in plaintext to output, this is standard practice
+            _aes.IV.CopyTo(cipherBuffer, 0);
+
+            int currentRead = offset;
+            int currentWrite = _aes.IV.Length;
+            if (_decryptor.CanTransformMultipleBlocks)
+            {
+                int len = length / BlockSizeInBytes;
+                //Make sure there is some for the last block
+                if (length % BlockSizeInBytes == 0) len--;
+                int count = _encryptor.TransformBlock(data, currentRead, len, cipherBuffer, currentWrite);
+            }
+            else
+            {
+                while (length - currentRead >= _encryptor.InputBlockSize)
+                {
+                    int encryptedCount = _encryptor.TransformBlock(data, currentRead, _encryptor.InputBlockSize, cipherBuffer, currentWrite);
+                    currentRead += encryptedCount;
+                    currentWrite += encryptedCount;
+                }
+            }
+
+            if (length - currentRead > 0)
+            {
+                //Encrypt and write last block to output
+                byte[] lastBytes = _encryptor.TransformFinalBlock(data, currentRead, length - currentRead);
+                lastBytes.CopyTo(cipherBuffer, currentWrite);
+                currentWrite += lastBytes.Length;
+            }
+
+            data = cipherBuffer;
+            offset = 0;
+            length = currentWrite;
+        }
+    }
+}
