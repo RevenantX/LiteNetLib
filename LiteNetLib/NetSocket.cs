0896ef58f35e3d0e81d83087d0eabac853448025
MichalPetryka <35800402+MichalPetryka@users.noreply.github.com>
2021-06-18 14:51:33 +0200

    Fix build issue (#446)

--- LiteNetLib/NetSocket.cs
+++ LiteNetLib/NetSocket.cs
@@ -581,7 +581,7 @@ namespace LiteNetLib
                         }
                         else
                         {
-#if NETCOREAPP || NETSTANDARD2_0_OR_GREATER
+#if NETCOREAPP || NETSTANDARD2_1 || NETSTANDARD2_1_OR_GREATER
                             remoteEndPoint.Address.TryWriteBytes(new Span<byte>(socketAddress, 8, 16), out _);
 #else
                             byte[] addrBytes = remoteEndPoint.Address.GetAddressBytes();
