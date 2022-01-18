01098685c37994f64ea8513b6fc7c3832f3cb11b
Mewiof <mewiof@gmail.com>
2022-01-18 22:52:44 +0300

    Update FastBitConverter.cs

--- LiteNetLib/Utils/FastBitConverter.cs
+++ LiteNetLib/Utils/FastBitConverter.cs
@@ -11,13 +11,30 @@ namespace LiteNetLib.Utils
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static unsafe void GetBytes<T>(byte[] bytes, int startIndex, T value) where T : unmanaged
         {
-            if (bytes.Length < startIndex + sizeof(T))
+            int size = sizeof(T);
+            if (bytes.Length < startIndex + size)
                 ThrowIndexOutOfRangeException();
 #if LITENETLIB_UNSAFELIB || NETCOREAPP3_1 || NET5_0 || NETCOREAPP3_0_OR_GREATER
             Unsafe.As<byte, T>(ref bytes[startIndex]) = value;
 #else
             fixed (byte* ptr = &bytes[startIndex])
+            {
+#if UNITY_ANDROID
+                // On some android systems, assigning *(T*)ptr throws a NRE if
+                // the ptr isn't aligned (i.e. if Position is 1,2,3,5, etc.).
+                // Here we have to use memcpy.
+                //
+                // => we can't get a pointer of a struct in C# without
+                //    marshalling allocations
+                // => instead, we stack allocate an array of type T and use that
+                // => stackalloc avoids GC and is very fast. it only works for
+                //    value types, but all blittable types are anyway.
+                T* valueBuffer = stackalloc T[1] { value };
+                UnsafeUtility.MemCpy(ptr, valueBuffer, size);
+#else
                 *(T*)ptr = value;
+#endif
+            }
 #endif
         }
 #else
