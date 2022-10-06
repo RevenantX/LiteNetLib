17c1fe4ed573be29ff1841c7aa383d7392ea81b7
Mewiof <105601570+Mewiof@users.noreply.github.com>
2022-10-06 23:15:54 +0300

    Revert "Update NetDataWriter.cs"
    This reverts commit 4594ac094e520857114457fc466720874a794568.

--- LiteNetLib/Utils/NetDataWriter.cs
+++ LiteNetLib/Utils/NetDataWriter.cs
@@ -128,7 +128,6 @@ namespace LiteNetLib.Utils
         }
 
         #region Put
-        /// <summary>Raw bytes</summary>
         public void Put(byte[] data, int offset, int length)
         {
             if (_autoResize) ResizeIfNeed(_position + length);
@@ -136,85 +135,34 @@ namespace LiteNetLib.Utils
             _position += length;
         }
 
-        /// <summary>Raw bytes</summary>
         public void Put(byte[] data) => Put(data, 0, data.Length);
 
-        public const byte TRUE = 1, FALSE = 0;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Put<T>(T value, int size)
+            where T : unmanaged
+        {
+            if (_autoResize) ResizeIfNeed(_position + size);
+            FastBitConverter.GetBytes(_data, _position, value);
+            _position += size;
+        }
 
         // ...the expressions presented in the following table are evaluated in compile time to the corresponding constant values and don't require an unsafe cont...
         // Source: https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/sizeof
-        public void PutByte(byte value)
-        {
-            if (_autoResize) ResizeIfNeed(_position + sizeof(byte));
-            FastBitConverter.GetBytes(_data, _position, value);
-            _position += sizeof(byte);
-        }
+        public void PutByte(byte value) => Put(value, sizeof(byte));
+        public void PutSByte(sbyte value) => Put(value, sizeof(sbyte));
 
-        public void PutSByte(sbyte value) {
-            if (_autoResize) ResizeIfNeed(_position + sizeof(sbyte));
-            FastBitConverter.GetBytes(_data, _position, value);
-            _position += sizeof(sbyte);
-        }
+        public const byte TRUE = 1, FALSE = 0;
 
         public void PutBool(bool value) => PutByte(value ? TRUE : FALSE);
-
-        public void PutShort(short value)
-        {
-            if (_autoResize) ResizeIfNeed(_position + sizeof(short));
-            FastBitConverter.GetBytes(_data, _position, value);
-            _position += sizeof(short);
-        }
-
-        public void PutUShort(ushort value)
-        {
-            if (_autoResize) ResizeIfNeed(_position + sizeof(ushort));
-            FastBitConverter.GetBytes(_data, _position, value);
-            _position += sizeof(ushort);
-        }
-
+        public void PutShort(short value) => Put(value, sizeof(short));
+        public void PutUShort(ushort value) => Put(value, sizeof(ushort));
         public void PutChar(char value) => PutUShort(value);
-
-        public void PutInt(int value)
-        {
-            if (_autoResize) ResizeIfNeed(_position + sizeof(int));
-            FastBitConverter.GetBytes(_data, _position, value);
-            _position += sizeof(int);
-        }
-
-        public void PutUInt(uint value)
-        {
-            if (_autoResize) ResizeIfNeed(_position + sizeof(uint));
-            FastBitConverter.GetBytes(_data, _position, value);
-            _position += sizeof(uint);
-        }
-
-        public void PutFloat(float value)
-        {
-            if (_autoResize) ResizeIfNeed(_position + sizeof(float));
-            FastBitConverter.GetBytes(_data, _position, value);
-            _position += sizeof(float);
-        }
-
-        public void PutLong(long value)
-        {
-            if (_autoResize) ResizeIfNeed(_position + sizeof(long));
-            FastBitConverter.GetBytes(_data, _position, value);
-            _position += sizeof(long);
-        }
-
-        public void PutULong(ulong value)
-        {
-            if (_autoResize) ResizeIfNeed(_position + sizeof(ulong));
-            FastBitConverter.GetBytes(_data, _position, value);
-            _position += sizeof(ulong);
-        }
-
-        public void PutDouble(double value)
-        {
-            if (_autoResize) ResizeIfNeed(_position + sizeof(double));
-            FastBitConverter.GetBytes(_data, _position, value);
-            _position += sizeof(double);
-        }
+        public void PutInt(int value) => Put(value, sizeof(int));
+        public void PutUInt(uint value) => Put(value, sizeof(uint));
+        public void PutFloat(float value) => Put(value, sizeof(float));
+        public void PutLong(long value) => Put(value, sizeof(long));
+        public void PutULong(ulong value) => Put(value, sizeof(ulong));
+        public void PutDouble(double value) => Put(value, sizeof(double));
 
         /// <summary>
         /// Note that "maxLength" limits the number of characters in a string, not its size in bytes.
@@ -253,7 +201,7 @@ namespace LiteNetLib.Utils
         public void Put<T>(T value) where T : INetSerializable => value.Serialize(this);
         #endregion
 
-        #region PutArray
+        #region Array
         public void PutArray(Array value, int size)
         {
             if (value == null)
@@ -270,22 +218,6 @@ namespace LiteNetLib.Utils
             _position += size;
         }
 
-        public void PutSByteArray(sbyte[] value, int offset, ushort length) {
-            if (_autoResize) ResizeIfNeed(_position + sizeof(ushort) + length);
-            FastBitConverter.GetBytes(_data, _position, length);
-            Buffer.BlockCopy(value, offset, _data, _position + sizeof(ushort), length);
-            _position += sizeof(ushort) + length;
-        }
-
-        public void PutByteArray(byte[] value, int offset, ushort length) {
-            if (_autoResize) ResizeIfNeed(_position + sizeof(ushort) + length);
-            FastBitConverter.GetBytes(_data, _position, length);
-            Buffer.BlockCopy(value, offset, _data, _position + sizeof(ushort), length);
-            _position += sizeof(ushort) + length;
-        }
-
-        // ...the expressions presented in the following table are evaluated in compile time to the corresponding constant values and don't require an unsafe cont...
-        // Source: https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/sizeof
         public void PutByteArray(byte[] value) => PutArray(value, sizeof(byte));
         public void PutSByteArray(sbyte[] value) => PutArray(value, sizeof(sbyte));
         public void PutBoolArray(bool[] value) => PutArray(value, sizeof(bool));
@@ -298,7 +230,7 @@ namespace LiteNetLib.Utils
         public void PutULongArray(ulong[] value) => PutArray(value, sizeof(ulong));
         public void PutDoubleArray(double[] value) => PutArray(value, sizeof(double));
 
-        /// <summary>Note that "stringMaxLength" limits the number of characters in a string, not its size in bytes.</summary>
+        /// <summary>Note that "maxLength" limits the number of characters in a string, not its size in bytes.</summary>
         public void PutStringArray(string[] value, int stringMaxLength)
         {
             if (value == null)
@@ -318,95 +250,198 @@ namespace LiteNetLib.Utils
         #endregion
 
         #region Obsolete
+
         [Obsolete("Use PutFloat instead")]
-        public void Put(float value) => PutFloat(value);
+        public void Put(float value)
+        {
+            PutFloat(value);
+        }
 
         [Obsolete("Use PutDouble instead")]
-        public void Put(double value) => PutDouble(value);
+        public void Put(double value)
+        {
+            PutDouble(value);
+        }
 
         [Obsolete("Use PutLong instead")]
-        public void Put(long value) => PutLong(value);
+        public void Put(long value)
+        {
+            PutLong(value);
+        }
 
         [Obsolete("Use PutULong instead")]
-        public void Put(ulong value) => PutULong(value);
+        public void Put(ulong value)
+        {
+            PutULong(value);
+        }
 
         [Obsolete("Use PutInt instead")]
-        public void Put(int value) => PutInt(value);
+        public void Put(int value)
+        {
+            PutInt(value);
+        }
 
         [Obsolete("Use PutUInt instead")]
-        public void Put(uint value) => PutUInt(value);
+        public void Put(uint value)
+        {
+            PutUInt(value);
+        }
 
         [Obsolete("Use PutChar instead")]
-        public void Put(char value) => PutChar(value);
+        public void Put(char value)
+        {
+            PutChar(value);
+        }
 
         [Obsolete("Use PutUShort instead")]
-        public void Put(ushort value) => PutUShort(value);
+        public void Put(ushort value)
+        {
+            PutUShort(value);
+        }
 
         [Obsolete("Use PutShort instead")]
-        public void Put(short value) => PutShort(value);
+        public void Put(short value)
+        {
+            PutShort(value);
+        }
 
         [Obsolete("Use PutSByte instead")]
-        public void Put(sbyte value) => PutSByte(value);
+        public void Put(sbyte value)
+        {
+            PutSByte(value);
+        }
 
         [Obsolete("Use PutByte instead")]
-        public void Put(byte value) => PutByte(value);
+        public void Put(byte value)
+        {
+            PutByte(value);
+        }
+
+        [Obsolete("Use PutSByteArray instead")]
+        public void PutSBytesWithLength(sbyte[] data, int offset, ushort length)
+        {
+            if (_autoResize)
+                ResizeIfNeed(_position + 2 + length);
+            FastBitConverter.GetBytes(_data, _position, length);
+            Buffer.BlockCopy(data, offset, _data, _position + 2, length);
+            _position += 2 + length;
+        }
+
+        [Obsolete("Use PutSByteArray instead")]
+        public void PutSBytesWithLength(sbyte[] value)
+        {
+            PutSByteArray(value);
+        }
+
+        [Obsolete("Use PutByteArray instead")]
+        public void PutBytesWithLength(byte[] data, int offset, ushort length)
+        {
+            if (_autoResize)
+                ResizeIfNeed(_position + 2 + length);
+            FastBitConverter.GetBytes(_data, _position, length);
+            Buffer.BlockCopy(data, offset, _data, _position + 2, length);
+            _position += 2 + length;
+        }
+
+        [Obsolete("Use PutByteArray instead")]
+        public void PutBytesWithLength(byte[] value)
+        {
+            PutByteArray(value);
+        }
 
         [Obsolete("Use PutBool instead")]
-        public void Put(bool value) => PutBool(value);
-
-        [Obsolete("Use PutSByteArray instead")]
-        public void PutSBytesWithLength(sbyte[] value, int offset, ushort length) => PutSByteArray(value, offset, length);
-
-        [Obsolete("Use PutSByteArray instead")]
-        public void PutSBytesWithLength(sbyte[] value) => PutSByteArray(value);
-
-        [Obsolete("Use PutByteArray instead")]
-        public void PutBytesWithLength(byte[] value, int offset, ushort length) => PutByteArray(value, offset, length);
-
-        [Obsolete("Use PutByteArray instead")]
-        public void PutBytesWithLength(byte[] value) => PutByteArray(value);
+        public void Put(bool value)
+        {
+            PutBool(value);
+        }
 
         [Obsolete("Use PutFloatArray instead")]
-        public void PutArray(float[] value) => PutFloatArray(value);
+        public void PutArray(float[] value)
+        {
+            PutFloatArray(value);
+        }
 
         [Obsolete("Use PutDoubleArray instead")]
-        public void PutArray(double[] value) => PutDoubleArray(value);
+        public void PutArray(double[] value)
+        {
+            PutDoubleArray(value);
+        }
 
         [Obsolete("Use PutLongArray instead")]
-        public void PutArray(long[] value) => PutLongArray(value);
+        public void PutArray(long[] value)
+        {
+            PutLongArray(value);
+        }
 
         [Obsolete("Use PutULongArray instead")]
-        public void PutArray(ulong[] value) => PutULongArray(value);
+        public void PutArray(ulong[] value)
+        {
+            PutULongArray(value);
+        }
 
         [Obsolete("Use PutIntArray instead")]
-        public void PutArray(int[] value) => PutIntArray(value);
+        public void PutArray(int[] value)
+        {
+            PutIntArray(value);
+        }
 
         [Obsolete("Use PutUIntArray instead")]
-        public void PutArray(uint[] value) => PutUIntArray(value);
+        public void PutArray(uint[] value)
+        {
+            PutUIntArray(value);
+        }
 
         [Obsolete("Use PutUShortArray instead")]
-        public void PutArray(ushort[] value) => PutUShortArray(value);
+        public void PutArray(ushort[] value)
+        {
+            PutUShortArray(value);
+        }
 
         [Obsolete("Use PutShortArray instead")]
-        public void PutArray(short[] value) => PutShortArray(value);
+        public void PutArray(short[] value)
+        {
+            PutShortArray(value);
+        }
 
         [Obsolete("Use PutBoolArray instead")]
-        public void PutArray(bool[] value) => PutBoolArray(value);
+        public void PutArray(bool[] value)
+        {
+            PutBoolArray(value);
+        }
 
         [Obsolete("Use PutStringArray instead")]
-        public void PutArray(string[] value) => PutStringArray(value);
+        public void PutArray(string[] value)
+        {
+            PutStringArray(value);
+        }
 
         [Obsolete("Use PutStringArray instead")]
-        public void PutArray(string[] value, int strMaxLength) => PutStringArray(value, strMaxLength);
+        public void PutArray(string[] value, int strMaxLength)
+        {
+            PutStringArray(value, strMaxLength);
+        }
 
         [Obsolete("Use PutIPEndPoint instead")]
-        public void Put(IPEndPoint endPoint) => PutIPEndPoint(endPoint);
+        public void Put(IPEndPoint endPoint)
+        {
+            PutIPEndPoint(endPoint);
+        }
 
         [Obsolete("Use PutString instead")]
-        public void Put(string value) => PutString(value);
+        public void Put(string value)
+        {
+            PutString(value);
+        }
 
+        /// <summary>
+        /// Note that "maxLength" only limits the number of characters in a string, not its size in bytes.
+        /// </summary>
         [Obsolete("Use PutString instead")]
-        public void Put(string value, int maxLength) => PutString(value, maxLength);
+        public void Put(string value, int maxLength)
+        {
+            PutString(value, maxLength);
+        }
+
         #endregion
     }
 }
