505e76c1190c4658a31e9adceff02712306a6d1e
Mustafa ÖZGÜR <root@mit.sh>
2023-12-09 10:58:14 +0300

    Multiple Interface problem
    fixed routing for multiple interface

--- LiteNetLib/NetManager.Socket.cs
+++ LiteNetLib/NetManager.Socket.cs
@@ -405,6 +405,7 @@ namespace LiteNetLib
             {
                 socket.ExclusiveAddressUse = !ReuseAddress;
                 socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, ReuseAddress);
+                socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.DontRoute, true);
             }
             catch
             {
