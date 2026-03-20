3f4cd4aa59a2eac37364bfae0941c74859476784
Lacyway <20912169+Lacyway@users.noreply.github.com>
2026-03-20 22:54:21 +0100

    Add docs for reject

--- LiteNetLib/ConnectionRequest.cs
+++ LiteNetLib/ConnectionRequest.cs
@@ -80,6 +80,16 @@ namespace LiteNetLib
             return _listener.OnConnectionSolved(this, null, 0, 0);
         }
 
+        /// <summary>
+        /// Rejects the connection request.
+        /// </summary>
+        /// <param name="rejectData">Optional user data to send along with the rejection packet.</param>
+        /// <param name="start">Offset in the <paramref name="rejectData"/> array.</param>
+        /// <param name="length">Length of the data to be sent from the <paramref name="rejectData"/> array.</param>
+        /// <param name="force">
+        /// If <see langword="true"/>, performs a "fire-and-forget" rejection without creating an internal peer.
+        /// If <see langword="false"/>, creates a temporary peer to ensure the rejection is delivered reliably.
+        /// </param>
         public void Reject(byte[] rejectData, int start, int length, bool force)
         {
             if (!TryActivate())
@@ -88,27 +98,62 @@ namespace LiteNetLib
             _listener.OnConnectionSolved(this, rejectData, start, length);
         }
 
+        /// <summary>
+        /// Rejects the connection reliably. Creates a temporary peer to handle packet loss.
+        /// </summary>
+        /// <param name="rejectData">Data to send with the rejection.</param>
+        /// <param name="start">Offset in the <paramref name="rejectData"/> array.</param>
+        /// <param name="length">Length of the data to be sent.</param>
         public void Reject(byte[] rejectData, int start, int length) =>
             Reject(rejectData, start, length, false);
 
+        /// <summary>
+        /// Rejects the connection immediately without reliability. 
+        /// Minimizes resource usage by not creating an internal peer.
+        /// </summary>
+        /// <param name="rejectData">Data to send with the rejection.</param>
+        /// <param name="start">Offset in the <paramref name="rejectData"/> array.</param>
+        /// <param name="length">Length of the data to be sent.</param>
         public void RejectForce(byte[] rejectData, int start, int length) =>
             Reject(rejectData, start, length, true);
 
+        /// <summary>
+        /// Rejects the connection immediately without reliability and without additional data.
+        /// </summary>
         public void RejectForce() =>
             Reject(null, 0, 0, true);
 
+        /// <summary>
+        /// Rejects the connection immediately without reliability.
+        /// </summary>
+        /// <param name="rejectData">Data to send with the rejection.</param>
         public void RejectForce(byte[] rejectData) =>
             Reject(rejectData, 0, rejectData.Length, true);
 
+        /// <summary>
+        /// Rejects the connection immediately without reliability using data from a <see cref="NetDataWriter"/>.
+        /// </summary>
+        /// <param name="rejectData">Writer containing the data to send.</param>
         public void RejectForce(NetDataWriter rejectData) =>
             Reject(rejectData.Data, 0, rejectData.Length, true);
 
+        /// <summary>
+        /// Rejects the connection reliably without additional data.
+        /// </summary>
         public void Reject() =>
             Reject(null, 0, 0, false);
 
+        /// <summary>
+        /// Rejects the connection reliably.
+        /// </summary>
+        /// <param name="rejectData">Data to send with the rejection.</param>
         public void Reject(byte[] rejectData) =>
             Reject(rejectData, 0, rejectData.Length, false);
 
+        /// <summary>
+        /// Rejects the connection reliably using data from a <see cref="NetDataWriter"/>.
+        /// </summary>
+        /// <param name="rejectData">Writer containing the data to send.</param>
         public void Reject(NetDataWriter rejectData) =>
             Reject(rejectData.Data, 0, rejectData.Length, false);
     }
